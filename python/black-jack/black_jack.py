0b92f311398e973a393221d1bf1503347db03c50
thomasbasoglu <thomasarif@hotmail.com>
2026-05-03 20:25:20 +0300

    added more projects

--- python/black-jack/black_jack.py
+++ python/black-jack/black_jack.py
@@ -20,8 +20,8 @@ def value_of_card(card):
         return 10
     if card == 'A':
         return 1
-    else:
-        return int(card) 
+    
+    return int(card) 
     
 
 
@@ -55,9 +55,11 @@ def value_of_ace(card_one, card_two):
     1.  'J', 'Q', or 'K' (otherwise known as "face cards") = 10
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
-    """
-
-    pass
+    """ 
+    if value_of_card(card_one) + value_of_card(card_two) > 10 or card_one == 'A' or card_two == 'A':
+        return 1
+    
+    return 11
 
 
 def is_blackjack(card_one, card_two):
@@ -70,8 +72,14 @@ def is_blackjack(card_one, card_two):
     2.  'A' (ace card) = 11 (if already in hand)
     3.  '2' - '10' = numerical value.
     """
-
-    pass
+    if value_of_card(card_one) + value_of_card(card_two) == 21:
+        return True
+    
+    if value_of_card(card_one) == 10 or value_of_card(card_two) == 10:
+        if card_one == 'A' or card_two == 'A':
+            return True
+    
+    return False
 
 
 def can_split_pairs(card_one, card_two):
@@ -80,8 +88,10 @@ def can_split_pairs(card_one, card_two):
     :param card_one, card_two: str - cards dealt.
     :return: bool - can the hand be split into two pairs? (i.e. cards are of the same value).
     """
-
-    pass
+    if value_of_card(card_one) == value_of_card(card_two):
+        return True
+    
+    return False
 
 
 def can_double_down(card_one, card_two):
@@ -90,5 +100,8 @@ def can_double_down(card_one, card_two):
     :param card_one, card_two: str - first and second cards in hand.
     :return: bool - can the hand can be doubled down? (i.e. totals 9, 10 or 11 points).
     """
-
-    pass
+    double_down = [9, 10, 11]
+    if value_of_card(card_one) + value_of_card(card_two) in double_down:
+        return True
+    return False
+    
