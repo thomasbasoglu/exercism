745264c97c9a29a20feda31a957d66b0fde41322
thomasbasoglu <thomasarif@hotmail.com>
2026-05-04 00:00:53 +0300

    Going to fixed some stuff added more

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,12 +1,17 @@
 """This is bobs responses """
 def response(hey_bob):
     """This func contains the responses"""
-    if hey_bob.upper() and hey_bob.endswith("?"):
-        return "Calm down, I know what I'm doing!"
-    if hey_bob.endswith("?"):
-        return "Sure"
-    if hey_bob.upper():
-        return "Whoa, chill out!" 
-    if hey_bob == "":
+    processed_input = hey_bob.strip()
+    is_yelling = processed_input.isupper()
+    is_question = processed_input.endswith("?")
+
+    if not processed_input:
         return "Fine. Be that way!"
-    return "Whatever"
+    if is_yelling and is_question:
+        return "Calm down, I know what I'm doing!"
+    if is_question:
+        return "Sure."
+    if is_yelling:
+        return "Whoa, chill out!" 
+
+    return "Whatever."
