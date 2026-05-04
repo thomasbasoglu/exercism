4018ddd32a1e6da09970c023941c18cab84e57b9
thomasbasoglu <thomasarif@hotmail.com>
2026-05-04 17:49:16 +0300

    Added the perfect numbers project

---
+++ python/perfect-numbers/perfect_numbers.py
@@ -0,0 +1,29 @@
+"""This function classifies numbers perfect abundant and deficient"""
+def classify(number):
+    """ A perfect number equals the sum of its positive divisors.
+
+    :param number: int a positive integer
+    :return: str the classification of the input integer
+    """
+    number = int(number)
+    aliquot_sum = 0
+    
+    if number <= 0:
+        raise ValueError("Classification is only possible for positive integers.")  
+    
+    
+    
+    for index in range(1, number):
+        if number % index == 0:
+            aliquot_sum+=index
+    
+    if number == aliquot_sum:
+        return "perfect"
+    
+    if number < aliquot_sum:
+        return "abundant"
+    
+    if number > aliquot_sum:
+        return "deficient"
+    
+    return None
