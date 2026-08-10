d52a1a2041869427bcecb5f6bcab730ef9701362
thomasbasoglu <thomasarif@hotmail.com>
2026-08-10 15:10:35 +0300

    Added Darts into exercism

---
+++ c/darts/darts.c
@@ -0,0 +1,19 @@
+#include "darts.h"
+
+uint8_t score(coordinate_t landing_position){
+    float distance = (landing_position.x * landing_position.x) + (landing_position.y * landing_position.y);
+
+    if(distance <= 1){
+        return 10;
+    }
+
+    if(distance <= 25){
+        return 5;
+    }
+
+    if(distance <= 100){
+        return 1;        
+    }
+
+    return 0;
+}
